bea16fdd39ec060a3b09e14baa36817d58240f21
Vincenzo Ampolo <vincenzo.ampolo@gmail.com>
2011-04-13 19:27:58 +0200

    Added mongoengine support

--- milo_app/milo_app/resources.py
+++ milo_app/milo_app/resources.py
@@ -1,3 +1,25 @@
+from mongoengine import *
+
+connect('milo')
+
 class Root(object):
     def __init__(self, request):
         self.request = request
+
+
+class User(Document):
+	email = StringField(required=True)
+	first_name = StringField()
+	last_name = StringField()
+	password = StringField()
+
+class Comment(EmbeddedDocument):
+	content = StringField()
+
+class Movie(Document):
+	title = StringField(required=True)
+	year = DateTimeField(required=True)
+	description = StringField()
+	trailer = URLField()
+	cover = FileField()
+	comments = ListField(EmbeddedDocumentField(Comment))
